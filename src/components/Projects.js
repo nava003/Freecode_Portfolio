248eb305cde0572be367733ed2f7553bd0a10c09
NavaHub <navaa003@gmail.com>
2024-01-19 00:36:29 -0600

    Working on Projects.js page component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,2 +1,15 @@
+import { CodeIcon } from "@heroicons/react/solid";
+import React from "react";
+import { projects } from "../data";
 
-export default function Projects() {}
+export default function Projects() {
+    return (
+        <section id="projects" className="text-gray-400 bg-gray-900 body-font">
+            <div className="">
+                <div className="">
+                    
+                </div>
+            </div>
+        </section>
+    )
+}
